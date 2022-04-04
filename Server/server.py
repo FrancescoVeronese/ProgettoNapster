7ae1dd73f74bd55fec2fc45e4ce632554f891ca2
FrancescoVeronese <francesco.veronese@iisviolamarchesini.edu.it>
2022-04-04 11:21:41 +0200

    Miglioramento di funzioni e l'uso delle stringhe

--- Server/server.py
+++ Server/server.py
@@ -28,22 +28,17 @@ def acceptLogin():
         #si crea un ID non presente nel DB allora valid=true e si esce dal while
         valid=True
     response="ALGI"+sessionID
-    print(response)
-    conn.send(response.encode())
+    return response
+    
 
-def acceptAdd():
+def acceptAdd(packet):
     #qui il server riceverà il file e lo salverà in una directory specifica
 
     #riceve cmd 4byte, sID 16byte, MD5 32byte,nome file 100byte
-    sID="" 
-    MD5=""
-    fileName=""
-    for x in range(4,20):
-        sID=sID+packet[x]
-    for y in range(20,52):
-        MD5=MD5+packet[y]
-    for z in range(52,152):
-        fileName=fileName+packet[z]
+    sID=packet[4:20]
+    MD5=packet[20:52]
+    fileName=packet[52:152]
+
     #si aggiunge: nome file, MD5, numero versione (autoincrementante), session ID nel DB (tabella file)
     #il sessionID presente come chiave esterna serve per eliminare tutti i file presenti nel DB appartenenti a un utente
     #non più connesso
@@ -53,62 +48,63 @@ def acceptAdd():
     #Per inviare al client il numero di file uguali contenuti nel server nella directory dove sono ospitati
     #si effettua una query SELECT COUNT DISTINCT in base all'MD5
     response="AADD"+directory
-    conn.send(response.encode())
+    
+    return response
 
-def acceptRemove():
-    sID="" 
-    MD5=""
-    for x in range(4,20):
-        sID=sID+packet[x]
-    for y in range(20,52):
-        MD5=MD5+packet[y]
+def acceptRemove(packet):
+    sID=packet[4:20]
+    MD5=packet[20:52]
     #rimuove dalla cartella di salvataggio il file
     #rimuove dalla tabella file, i record che hanno session id=sID e MD5=MD5
     directory="" #3B
     response="ADEL"+directory
-    conn.send(response.encode())
-def findFile():
-    sID="" 
-    for x in range(4,20):
-        sID=sID+packet[x]
+    return response
+    
+def findFile(packet):
+    sID=packet[4:20]
+
     #manderà al peer numero di file con MD5 uguale
     #riporterà 
     numfile=0
     #invia al peer gli MD5 dei file con nome file che comprende il nome inviato dal peer stesso
+    response=""
+    return response
 
-def acceptLogout():
-    sID="" 
-    for x in range(4,20):
-        sID=sID+packet[x]
+def acceptLogout(packet):
+    sID=packet[4:20]
     #ELIMINA file dalla directory
     #query SQL che conta i file presenti nel DB con quel sID e li elimina
     numfile=0
     response="ALGO"+str(numfile)
-    conn.send(response.encode())
+    return response
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 s.bind(("",80))
 s.listen(10)
 CommandAction=""
+response=""
 while True:
     conn,addr=s.accept()
     packet=conn.recv(1024).decode()
-    commandAction=""
+    
     #si prendono i primi 4 byte(caratteri) della stringa inviata dal client, che identificano il tipo di comando inviato
-    for x in range(0,4):
-        commandAction=commandAction+packet[x]
+    commandAction=packet[0:4]
 
     match commandAction:
         case 'LOGI' : 
-            acceptLogin() #manda al client una stringa di 20 byte al peer 4 cmd, 16 SID #X
+            response=acceptLogin()
+            conn.send(response.encode()) #manda al client una stringa di 20 byte al peer 4 cmd, 16 SID #X
         case 'ADDF':
-            acceptAdd() #risponde con la conferma dell'inserimento nel DB del file,
+            response=acceptAdd(packet)
+            conn.send(response.encode()) #risponde con la conferma dell'inserimento nel DB del file,
                         # e del numero di file con stesso MD5 preesnti
         case 'DELF':
-            acceptRemove() #risponde con la conferma della cancellazione del file
+            response=acceptRemove(packet)
+            conn.send(response.encode()) #risponde con la conferma della cancellazione del file
         case 'FIND':
-            findFile()
+            response=findFile()
+            conn.send(response.encode())
         case 'LOGO':
-            acceptLogout()
-
+            response=acceptLogout()
+            conn.send(response.encode())
