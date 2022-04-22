9f92d5ea413478949cd5016b13ae36fd0aad2796
FrancescoVeronese <francesco.veronese@iisviolamarchesini.edu.it>
2022-04-22 16:53:51 +0200

    test

--- Client/client.py
+++ Client/client.py
@@ -3,9 +3,6 @@ import hashlib
 import os
 import random
 
-#INDICAZIONI GENERALI:
-#NON usare cancelletti per riempire posti vuoti in stringhe, lasciare lo spazio vuoto (risolto)
-#La parte di DOWNLOAD va sviluppata dopo il 14/04/2022, per il testing ora è lasciata a commento
 
 #calcolo md5 metodo
 def calcmd5(fname):
@@ -31,6 +28,7 @@ def aggiungi(sessionID):
     nameFile_send=nameFile.ljust(100)
     response="ADDF"+str(sessionID)+str(md5)+str(nameFile_send)
     return response
+    
 def delete(sessionID):
     nameFile = input("Scrivere nome file")
     md5 = calcmd5(nameFile)
@@ -43,21 +41,20 @@ def find(sessionID):
     string=string.ljust(20)
     response="FIND"+str(sessionID)+str(string)
     return response
-    
+
+     
 def logout(sessionID):
     response="LOGO"+str(sessionID)
     return response
-'''
+
 def cliedownload(sessionID):
     response="RETR"+str(sessionID)
     return response
-'''
-'''
+
 def servdownload(sessionID):
     response="RREG"+str(sessionID)
     return response
-'''
-""" 
+ 
 def download(serverhost, serverport, filecode):  # funzione per scaricare un file da un altro peer
         s2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # connessione al peer
         s2.connect((serverhost,serverport))
@@ -77,9 +74,9 @@ def download(serverhost, serverport, filecode):  # funzione per scaricare un fil
                 data = s2.recv(1024)
                 totalRecv += 1024
                 f.write(data)
-            print("File is downloaded Successfully")
+            print("File scaricato con successo")
             s2.close()
-"""
+
 ip = ""
 remoteip="25.13.127.60"
 porta = 80
@@ -104,7 +101,7 @@ s1.listen(10)
 pid = os.fork()
 while True:
     if pid > 0:
-        scelta = input("Scelta azione:\n 1 per Aggiungere un file\n 2 per Rimuovere un file\n 3 per Ricercare un file\n 4 per Scaricare un file\n 5 per fare Logout")
+        scelta = int(input("Scelta azione:\n 1 per Aggiungere un file\n 2 per Rimuovere un file\n 3 per Ricercare un file\n 4 per Scaricare un file\n 5 per fare Logout"))
         if(scelta==1):
                 response = aggiungi(sessionID)
                 s.send(response.encode())
@@ -117,19 +114,34 @@ while True:
                 
             else:
                 if(scelta==3):
+
                     response = find(sessionID)
                     s.send(response.encode())
+                    risultato=s.recv(1024).decode()
+                    numfile=risultato[4:7]
+                    print("Il server ha trovato ha i seguenti file...\n")
+                    for n in numfile:
+                        s.recv(1024).decode()
+                        filemd5=risultato[0:32]
+                        filename=risultato[32:132]
+                        copies=risultato[132:135]
+                        ippeer=risultato[135:150]
+                        portpeer=risultato[150:155]
+
+                        peer=(filemd5,filename,copies,ippeer,portpeer)
+                        print("%s\n,%s\n,%s\n,%s:%s",peer)
+
                 else:
                     if(scelta==4):
-                        '''
-                        port = input("Inserisci porta del peer")
+                        
+                        
                         host = input("Inserisci indirizzo del peer")
                         filename = input("Inserisci nome file")
                         filecode = calcmd5(filename)
-                        download(host, port, filecode)
+                        download(host, porta, filecode)
                         response = servdownload(sessionID)
-                        s.send(response.encode()) # informo il server dello scaricamento
-                        '''
+                        s.send(response.encode()) # informo il server del download
+                        
                     else:
                         if(scelta==5):
                             response = logout(sessionID)
--- Server/server.py
+++ Server/server.py
@@ -3,8 +3,15 @@ from random import *
 import sys
 import mysql.connector
 from requests import Session 
-#ERRORE RIGA 33 "unknown column in 'Field List'"
 
+
+#aggiunge n 0 prima della stringa data come parametro fino ad avere una lunghezza uguale a dim
+def Resize(stringa, dim):
+    tmp=""
+    for n in range(0,dim-len(stringa)):
+       tmp+="0"
+    tmp+=stringa
+    return tmp
 def dataSender(send):
     conn.send(str(send).encode())
 def acceptLogin():
@@ -34,31 +41,13 @@ def acceptLogin():
         mydb.commit()
     except BaseException as errore: #Tutte le eccezioni ereditano da Baseexception
         print("ERRORE:"+errore.msg)
-        sessionID="0000000000000000"    
-        ''' Al posto di cercare il numero di utenti con quel SID o altro, il metodo pià semplice è lasciare un
-        eccezione, che se trovata, ritorna tutti zero al client
-        try:
-            cursor.execute(f"SELECT COUNT(*) FROM UTENTE WHERE SID='{sessionID}'") 
-            queryresult=cursor.fetchall()[0][0]
-            print(f"Il SID creato è presente nel DB {queryresult} volte")
-            if(int(queryresult)==0):
-                print(f"Il SID non è presente nel DB, invio del SID al client...\n")
-                valid=True
-                valori=(sessionID,IPAddress,Port)
-                cursor.execute("INSERT INTO UTENTE (SID,IP,PORT) VALUES (%s,%s,%s)",valori)
-                mydb.commit()
-            else:
-                print(f"SID già presente nel DB, generazione di un altro SID...\n")       
-        except:
-            print("ERRORE CONTROLLO/INSERIMENTO SID NEL DATABASE")
-            sessionID='0000000000000000' '''
+        sessionID="0000000000000000"
         
     response="ALGI"+sessionID
     return response
     
 
 def acceptAdd(packet):
-
     #riceve cmd 4byte, sID 16byte, MD5 32byte,nome file 100byte
     sID=packet[4:20]
     MD5=packet[20:52]
@@ -75,18 +64,16 @@ def acceptAdd(packet):
         print("ERRORE nell'aggiunta del file:"+errore.msg)
     
     cursor.execute("SELECT COUNT (SID) FROM ARCHIVIO WHERE MD5=%s",MD5) #si conta quante volte il file sia presente nel DB
-    copy=cursor.fetchall()[0][0]
-    copy=copy.ljust(3)
+    copy=cursor.fetchall()
+    copy=Resize(str(copy[0][0]),3)
     #il sessionID presente come chiave esterna serve per eliminare tutti i file presenti nel DB appartenenti a un utente
     #non più connesso
     response="AADD"+copy
-    
     return response
 
 def acceptRemove(packet):
     sID=packet[4:20]
     MD5=packet[20:52]
-    #rimuove dalla cartella di salvataggio il file
     #rimuove dalla tabella file, i record che hanno session id=sID e MD5=MD5
     directory="" #3B
     response="ADEL"+directory
@@ -138,7 +125,7 @@ while True:
             response=acceptAdd(packet)
             print(f"La risposta al client è {response}")
             dataSender(response) #risponde con la conferma dell'inserimento nel DB del file,
-            # e del numero di file con stesso MD5 preesnti
+            # e del numero di file con stesso MD5 presenti
         else:
             if(commandAction=='DELF'):
                 response=acceptRemove(packet)
